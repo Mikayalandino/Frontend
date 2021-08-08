4f344b383863f622bb40d7d1c43f5e1ed0490256
Mika <Micaelayalandino@gmail.com>
2021-08-07 23:32:04 -0300

    se agregaron ejs

--- Clases/Módulo 2 -JavaScript/Clase N°29 - Condicionales II/Ejercicios/3. Puede avanzar/main.js
+++ Clases/Módulo 2 -JavaScript/Clase N°29 - Condicionales II/Ejercicios/3. Puede avanzar/main.js
@@ -1,9 +1,10 @@
 const puedeAvanzar = (color) => {
-    if(color === "verde"){
-    return true;
-    }
-    else{
+  if(color === "verde"){
+  return true;
+  }
+  else if (color === "amarillo"){
     return false;
-    }
+  }
+  else if(color === "rojo")
+    return false;
 }
-
