70b821cb5b7989d3a352109bcb5f3e4ecc9b3397
Mika <Micaelayalandino@gmail.com>
2021-10-04 18:18:12 -0300

    clases y ejs

---
+++ Clases/Módulo 5 - Estructuras de datos/Clase N°61 - Objetos I/Ejercicios/Ejercicio 6/main.js
@@ -0,0 +1,12 @@
+const user = {
+    firstName: "Mika",
+    lastName: "Asd",
+    email: "asd@gmail.com",
+    age: 27,
+}
+
+user.tecnologiasConocidas = ["html", "css", "sass", "javascript"];
+user.sabeProgramar = true
+
+
+console.log(saludarConObjeto)
--- Clases/Módulo 5 - Estructuras de datos/Clase N°61 - Objetos/Ejercicios/Ejercicio 6/main.js
+++
@@ -1,7 +0,0 @@
-const user = `Mika`
-
-const saludarConObjeto = (objeto) => {
-    return `Hola ${user}`
-}
-
-console.log(saludarConObjeto(user))
